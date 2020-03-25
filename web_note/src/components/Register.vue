7dd8339c0dd13c38e7cb6bf570384f72adecf55f
sipoohe <1473427831@qq.com>
2020-03-25 14:29:14 +0800

    init

---
+++ web_note/src/components/Register.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<el-form ref="form" :model="registerForm" label-width="80px">
+			<el-form-item label="用户名">
+				<el-input placeholder="请输入用户名" v-model="registerForm.username"></el-input>
+			</el-form-item>
+			<el-form-item label="密码">
+				<el-input placeholder="请输入密码" v-model="registerForm.password" show-password></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="onSubmit">立即创建</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "Register",
+	    data() {
+            return {
+                registerForm:{
+                    username: '',
+	                password: '',
+                }
+            }
+	    },
+	    methods: {
+            onSubmit() {
+
+            }
+	    }
+    }
+</script>
+
+<style scoped>
+
+</style>
